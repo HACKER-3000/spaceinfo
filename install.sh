ad64092ff01e97be5b584b25e809ee5452c7cc35
HACKER-3000 <hacker3000@posteo.org>
2018-06-11 16:55:13 +0200

    editted errors

--- install.sh
+++ install.sh
@@ -5,4 +5,8 @@ mkdir /etc/spaceinfo
 
 cp spaceinfo.service //lib/systemd/system/spaceinfo.service
 
+cd /opt/spaceinfo
+
+git clone https://github.com/vspaceone/spaceinfo.git
+
 systemctl daemon-reload
